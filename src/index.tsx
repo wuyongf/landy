53c627455acca9721bacaf5f6857ab4a7cde941e
yf <wuyongf@gmail.com>
2024-12-15 21:28:46 +0800

    test7

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 import { I18nextProvider } from "react-i18next";
 import "antd/dist/antd.css";
 
@@ -8,11 +8,11 @@ import Router from "./router";
 import i18n from "./translation";
 
 const App = () => (
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
+  <HashRouter>
     <I18nextProvider i18n={i18n}>
       <Router />
     </I18nextProvider>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 const root = ReactDOM.createRoot(document.getElementById("root")!);
